d62ca768d784a42636b8a1fd1539b6b6a9ed9f2f
Gema Dodi Pranata <gemadp01@gmail.com>
2025-09-20 22:14:51 +0700

    feat(lib): add mail helper function

---
+++ src/lib/mail.js
@@ -0,0 +1,35 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD,
+  },
+});
+
+transporter.verify((err, success) => {
+  if (err) {
+    console.error("Nodemailer error:", err);
+  } else {
+    console.log("Nodemailer ready to send emails");
+  }
+});
+
+export const sendEmail = async ({ to, subject, html }) => {
+  try {
+    const info = await transporter.sendMail({
+      from: `"Product Catalog" <${process.env.GMAIL_USER}>`,
+      to,
+      subject,
+      html,
+    });
+    console.log("Email sent:", info.messageId);
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
