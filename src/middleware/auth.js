4bf0b27f2c511ac4bd908105e8902201625ff675
Gema Dodi Pranata <gemadp01@gmail.com>
2025-09-22 08:18:45 +0700

    feat(auth): add JWT token verification middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const auth = (req, res, next) => {
+  const JWT_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
+  // ambil token dari header -> Bearer[0] token[1]
+  const token = req.header("Authorization")?.split(" ")[1]; // "Bearer token"
+
+  // cek token
+  if (!token)
+    return res.status(401).json({ message: "No token, authorization denied" });
+
+  try {
+    // cek token validasi
+    const verified = jwt.verify(token, JWT_SECRET);
+
+    // tambahkan user dari payload token
+    req.user = verified;
+
+    next();
+  } catch (err) {
+    res.status(400).json({ message: "Token is not valid" });
+  }
+};
+
+export default auth;
