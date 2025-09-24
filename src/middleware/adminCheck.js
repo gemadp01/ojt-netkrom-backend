87dd42a05ac30a16bced511894f1d4454515b474
Gema Dodi Pranata <gemadp01@gmail.com>
2025-09-24 13:31:52 +0700

    feat(middleware): add admin check

---
+++ src/middleware/adminCheck.js
@@ -0,0 +1,8 @@
+const adminCheck = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Access denied" });
+  }
+  next();
+};
+
+export default adminCheck;
